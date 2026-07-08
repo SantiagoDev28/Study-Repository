83765ac4d3888b74e61bd7ca50fc3cf1d1a5d947
santiagodev28 <shurtado308@gmail.com>
2026-07-08 15:31:22 -0500

    docs(ts-types): add TypeScript types explanation and code example.

---
+++ 02-practice/typescript/src/types/types.ts
@@ -0,0 +1,10 @@
+// Explicit type annotation
+let myName: string = "Santiago Hurtado";
+console.log(`Hi, My name is ${myName}`)
+
+// Type inference
+let myAge = 20
+console.log(`and I'm ${myAge} years old` )
+
+// ERROR: Cannot assing a string to a number.
+// myAge = 'Twenty'
